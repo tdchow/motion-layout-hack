9035aa6aeb651630877c755c5442acb1109e3c4e
Tim Chow <tichow@paypal.com>
2020-11-06 16:58:46 -0600

    Added MotionLayout examples

--- app/src/main/java/com/example/motionlayouthack/MainActivity.kt
+++ app/src/main/java/com/example/motionlayouthack/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.motionlayouthack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.triangleExampleButton).setOnClickListener {
+            startActivity(Intent(this, TriangleExampleActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.widgetExampleButton).setOnClickListener {
+            startActivity(Intent(this, WidgetExampleActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/motionlayouthack/WidgetExampleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.motionlayouthack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WidgetExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget_example)
+    }
+}
